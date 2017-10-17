6ab7d0ee7ed4d23fc7621c16412479c030af7f87
David Yu <yuyang545262477@gmail.com>
2017-10-17 22:20:14 +0800

    simple node module

--- node-example/index.js
+++ node-example/index.js
@@ -1,7 +1,9 @@
-let rect = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-};
+let rect = require('./rectangle');
+
+// let rect = {
+//     perimeter: (x, y) => (2 * (x + y)),
+//     area: (x, y) => (x * y)
+// };
 
 function solveRect(l, b) {
     console.log(`solving for rectangle with l = ${l} and b = ${b}`);
