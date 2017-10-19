b87ad5ac7fb35c025976e816a729e08e0ff7040c
David Yu <yuyang545262477@gmail.com>
2017-10-19 23:51:43 +0800

    node http example 2

--- node-http/index.js
+++ node-http/index.js
@@ -2,12 +2,58 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
+const fs = require('fs');
+const path = require('path');
 
-    res.stateCode = 200;
+const server = http.createServer((req, res) => {
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method !== 'GET') return notSupportGet();
+
+    let fileUrl = renderFileUrl();
+    let filePath = renderFilePath();
+
+    if (checkFileType() !== '.html') return FourZeroFour();
+    if (!checkExist()) return FourZeroFour();
+
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(`<html><body><h1>Hello , World!</h1></body></html>`)
+
+    // noinspection JSUnresolvedFunction
+    fs.createReadStream(filePath).pipe(res);
+
+    function renderFileUrl() {
+        return req.url === '/' ? '/index.html' : req.url;
+    }
+
+    function renderFilePath() {
+        return path.resolve('./public' + fileUrl);
+    }
+
+    function checkExist() {
+        return fs.existsSync(filePath);
+    }
+
+    function checkFileType() {
+        return path.extname(filePath);
+    }
+
+    function notSupportGet() {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method +
+            ' not supported</h1></body></html>');
+    }
+
+    function FourZeroFour() {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + fileUrl +
+            ' not a HTML file</h1></body></html>'
+        );
+    }
+
+
 });
 
 server.listen(port, hostname, () => {
