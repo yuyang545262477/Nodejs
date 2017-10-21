8503578371f7c74ce9ac56506e47445818621ea1
David Yu <yuyang545262477@gmail.com>
2017-10-21 17:18:10 +0800

    add route example one

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3001;
@@ -8,6 +9,45 @@ const port = 3001;
 const app = express();
 
 app.use(morgan('dev'));
+
+
+//router place
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end(`Will send details of dish:  ${req.params.dishId} to you !`);
+});
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /dishes/ ${req.params.dishId}`);
+});
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write(`Updating the dish: ${req.params.dishId} \n`);
+    res.end(`Will Update the dish: ${req.body.name} with details ${req.body.description}`);
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all dishes');
+});
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end(`Deleting dish: ${req.params.dishId}`);
+});
+
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     console.log(req.headers);
@@ -15,6 +55,7 @@ app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
+
 
 const server = http.createServer(app);
 
